06de5be703f63c3c25757ad8c9324b302087de7f
José Daniel <josedanq100@gmail.com>
2018-07-18 17:14:46 -0400

    generating files to gh-page

--- config/index.js
+++ config/index.js
@@ -50,7 +50,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
+    assetsPublicPath: 'https://josedan10.github.io/josedan.js/',
 
     /**
      * Source Maps
