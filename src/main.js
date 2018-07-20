da7a6f5c94527c682d9139038630fc59dcd81ba9
José Daniel <josedanq100@gmail.com>
2018-07-19 20:44:51 -0400

    AOS bug fixed

--- src/main.js
+++ src/main.js
@@ -1,12 +1,16 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { faBars } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 
 import App from './App';
 import router from './router';
+
+AOS.init();
 
 Vue.config.productionTip = false;
 
