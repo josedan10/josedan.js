37f4b0f31eaa7bcfce48987e66a38a42aad5dacd
José Daniel <josedanq100@gmail.com>
2018-07-20 03:11:14 -0400

    Testing 404

---
+++ src/components/Error404.vue
@@ -0,0 +1,9 @@
+<template>
+  Error 404 Page Not Found :c
+</template>
+
+<script>
+  export default {
+    name: 'Error404'
+  }
+</script>
--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,7 @@ import Vue from 'vue';
 import Router from 'vue-router';
 import Home from '@/components/pages/Home';
 import Contact from '@/components/pages/Contact';
-// import
+import Error404 from '@/components/Error404';
 
 Vue.use(Router);
 
@@ -18,11 +18,11 @@ export default new Router({
       path: '/Contact',
       name: 'Contact',
       component: Contact
+    },
+    {
+      path: '*',
+      name: '404',
+      component: Error404
     }
-    // {
-    //   path: '*',
-    //   name: '404',
-    //   component: Error404
-    // }
   ]
 });
