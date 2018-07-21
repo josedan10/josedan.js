b27d1c5df958fece4b1f3b29f0f8dba1b95bcc1e
José Daniel <josedanq100@gmail.com>
2018-07-21 12:25:52 -0400

    Change 404 image by png

--- src/components/pages/Error404.vue
+++ src/components/pages/Error404.vue
@@ -1,11 +1,14 @@
 <template>
   <div class="banner d-flex error404">
 
-    <h1>Error 404 Page Not Found :c</h1>
+    <h1 class="col col-12 col-md-6"><span class="text-danger">Error 404.</span> Page Not Found</h1>
+    <img class="col col-12 col-md-6" src="../../assets/meBinary.png" alt="">
+
   </div>
 </template>
 
 <script>
+
   export default {
     name: 'Error404'
   };
