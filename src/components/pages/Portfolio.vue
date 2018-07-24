f541eb628c44f503495e2a5485ddd57a0fd91ec9
José Daniel <josedanq100@gmail.com>
2018-07-24 16:36:04 -0400

    Adding a simple view for portfolio meanwhile

---
+++ src/components/pages/Portfolio.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+      <div class="d-flex banner portfolio">
+
+        <h1>Actually I am building this page. Coming soon I'll upload my projects :)</h1>
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Portfolio'
+};
+
+</script>
--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,7 @@ import Router from 'vue-router';
 import Home from '@/components/pages/Home';
 import Contact from '@/components/pages/Contact';
 import Error404 from '@/components/pages/Error404';
+import Portfolio from '@/components/pages/Portfolio';
 
 Vue.use(Router);
 
@@ -17,6 +18,11 @@ export default new Router({
       path: '/Contact',
       name: 'Contact',
       component: Contact
+    },
+    {
+      path: '/Portfolio',
+      name: 'Portfolio',
+      component: Portfolio
     },
     {
       path: '*',
